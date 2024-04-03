930d3145c7efd4881ce8910d78cb395d6797024a
veragfernandes <127191676+veragfernandes@users.noreply.github.com>
2024-04-03 09:08:03 +0100

    Nivel2

--- AirTransportation.java
+++ AirTransportation.java
@@ -7,15 +7,46 @@
  */
 public class AirTransportation extends Transport{
     private String name;
-    private int numberOfContaineres;
+    private int numberOfContainers;
     /**
      * Construtor para objetos da classe AirTransportation
      */
-    public AirTransportation()
+    public AirTransportation(String name, int numberOfContainers)
     {
        super();
        setFees(4.0);
        
+       if(name!= null){
+           this.name= name;
+       } else{
+           this.name = " ";
+       }
+       
+       if(numberOfContainers>=0){
+           this.numberOfContainers = numberOfContainers;
+       } else {
+           this.numberOfContainers = 0;
+       }
+    }
+    
+    public String getName(){
+        return name;
+    }
+    
+    public void setName(String name){
+        if(name != null){
+            this.name = name;
+        }
+    }
+    
+    public int getNumberOfContainers(){
+        return numberOfContainers;
+    }
+    
+    public void setNumberOfContainers(int numberOfContainers){
+        if(numberOfContainers>=0){
+            this.numberOfContainers= numberOfContainers;
+        }
     }
 
 }
--- GroundTransportation.java
+++ GroundTransportation.java
@@ -11,10 +11,26 @@ public class GroundTransportation extends Transport{
     /**
      * Construtor para objetos da classe GroundTransportation
      */
-    public GroundTransportation()
+    public GroundTransportation(String licensePlate)
     {
         super();
         setFees(3.0);
-    }
+        
+        if(licensePlate!= null){
+            this.licensePlate= licensePlate;
+        } else {
+            this.licensePlate= " ";
+        }
 
+    }
+    
+    public String getLicensePlate(){
+        return licensePlate;
+    }
+    
+    public void setLicensePlate(String licensePlate){
+        if(licensePlate!= null){
+            this.licensePlate= licensePlate;
+        }
+    }
 }
