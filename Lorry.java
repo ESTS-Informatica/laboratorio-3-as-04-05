e8d7fcd1a562b4d75535dadec36e9486104730cf
veragfernandes <127191676+veragfernandes@users.noreply.github.com>
2024-04-03 09:18:40 +0100

    Nivel 4 inicio

---
+++ Lorry.java
@@ -0,0 +1,17 @@
+
+/**
+ * Escreva uma descrição da classe Lorry aqui.
+ * 
+ * @author (seu nome) 
+ * @version (um número da versão ou uma data)
+ */
+public class Lorry extends GroundTransportation {
+   
+    /**
+     * Construtor para objetos da classe Lorry
+     */
+    public Lorry()
+    {
+        
+    }
+}
---
+++ Van.java
@@ -0,0 +1,18 @@
+
+/**
+ * Escreva uma descrição da classe Van aqui.
+ * 
+ * @author (seu nome) 
+ * @version (um número da versão ou uma data)
+ */
+public class Van extends GroundTransportation
+{
+    /**
+     * Construtor para objetos da classe Van
+     */
+    public Van()
+    {
+        
+    }
+
+}
