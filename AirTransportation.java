3acb66305d9a734c524c493be228fc0cd46cc160
veragfernandes <127191676+veragfernandes@users.noreply.github.com>
2024-04-03 08:56:59 +0100

    Nivel 1

---
+++ AirTransportation.java
@@ -0,0 +1,21 @@
+
+/**
+ * Escreva uma descrição da classe AirTransportation aqui.
+ * 
+ * @author (seu nome) 
+ * @version (um número da versão ou uma data)
+ */
+public class AirTransportation extends Transport{
+    private String name;
+    private int numberOfContaineres;
+    /**
+     * Construtor para objetos da classe AirTransportation
+     */
+    public AirTransportation()
+    {
+       super();
+       setFees(4.0);
+       
+    }
+
+}
---
+++ GroundTransportation.java
@@ -0,0 +1,20 @@
+
+/**
+ * Escreva uma descrição da classe GroundTransportation aqui.
+ * 
+ * @author (seu nome) 
+ * @version (um número da versão ou uma data)
+ */
+public class GroundTransportation extends Transport{
+    private String licensePlate;
+    
+    /**
+     * Construtor para objetos da classe GroundTransportation
+     */
+    public GroundTransportation()
+    {
+        super();
+        setFees(3.0);
+    }
+
+}
